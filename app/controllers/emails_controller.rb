c64f753f5b9328fac900d5436eda246017952e8a
RomualdPetit <romuol@hotmail.fr>
2021-11-25 09:59:55 +0100

    Romu, Lucas, Vali, Quentin, Justin

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,40 @@
+require 'faker'
+
+class EmailsController < ApplicationController
+
+    def index
+      @emails = Email.all
+    end
+
+    def create
+      @email = Email.create(object: Faker::Book.title, body: Faker::Book.author)
+  
+     
+    end
+
+    def show
+      @email = Email.find(params[:id])
+
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
+    end
+
+    def destroy
+      @email = Email.find(params[:id])
+      @email.destroy
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {}
+      end
+    end
+  
+    private
+  
+  def emails_params
+    params.permit(:object, :body)
+  end
+  
+  
+end
