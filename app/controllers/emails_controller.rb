297ca996b81ee21cd719d9943c38fb4c4bd6c063
Roronoha69 <mbodje.justin@gmail.com>
2021-11-25 11:19:34 +0100

    btn supprimer

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -24,9 +24,10 @@ class EmailsController < ApplicationController
     def destroy
       @email = Email.find(params[:id])
       @email.destroy
+
       respond_to do |format|
         format.html { redirect_to root_path }
-        format.js {}
+        format.js { redirect_to root_path }
       end
     end
   
